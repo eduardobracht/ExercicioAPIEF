f768bb40bda996a514df27db37ba7779fb9a6e14
Leandro <leandro.rwd@gmail.com>
2017-05-23 21:43:55 -0300

    first commit

---
+++ ProdutosWebApi/Controllers/WebapiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProdutosWebApi.Controllers
+{
+    public class WebapiController : ApiController
+    {
+        //IEnumerable<Product> GetAllProducts;
+
+        //Product GetProduct(int id);
+
+        //IEnumerable<Product> GetProductsByCategory(string category);
+
+        //HttpResponseMessage PostProduct(Product item);
+
+        //void PutProduct(int id, Product product);
+
+        //void DeleteProduct(int id);
+
+
+    }
+}
